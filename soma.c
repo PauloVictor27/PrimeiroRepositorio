43494232961cf95d50aa81a94f15c4a7542ce55a
Paulo Victor de Aguiar <PauloVictor27@users.noreply.github.com>
2015-08-03 17:33:45 -0300

    Programa para exercício de github

---
+++ soma.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main{
+	int numero1
+	int numero2
+	int soma
+	
+	printf(Digite o primeiro numero: )
+	scanf(numero1)
+	printf(Digite o segundo numero: )
+	scanf(numero2)
+	
+	soma = numero1 e numero2
+	printf(A soma dos numeros eh: soma)
+	
+	return 0
+	}
