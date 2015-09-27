37f64c26de9938688f53eb69709d0f8536b86ed2
bruoliveeira <oliver.brunaa@hotmail.com>
2015-09-27 16:43:23 -0300

    bruna

--- soma.c
+++ soma.c
@@ -1,18 +1,19 @@
-include stdio.h
-include stdlib.h
+#include <stdio.h>
+#include <stdlib.h>
 
-int main{
-	int numero1
-	int numero2
-	int soma
+int main () {
 	
-	printf(Digite o primeiro numero: )
-	scanf(numero1)
-	printf(Digite o segundo numero: )
-	scanf(numero2)
+	int numero1;
+	int numero2;
+	int soma;
 	
-	soma = numero1 e numero2
-	printf(A soma dos numeros eh: soma)
+	printf("Digite o primeiro numero: " );
+	scanf("%i",&numero1);
+	printf("Digite o segundo numero: ");
+	scanf("%i", &numero2);
 	
-	return 0
+	soma = numero1 + numero2;
+	printf("A soma dos numeros eh: %i", soma);
+	
+	return 0;
 	}
